189c95644858d603045bd53245fc32225f2bed67
hosshinon <hosshinon@gmail.com>
2024-03-17 00:15:47 +0900

    bug/CIの設定修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,7 +64,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  #分離したコンテナからアクセスするための設定
+  # 分離したコンテナからアクセスするための設定
   config.hosts << "host.docker.internal"
-
 end
