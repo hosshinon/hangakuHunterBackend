eca7951ecb89622ee1d314e3453ea3ed9ae40790
hosshinon <hosshinon@gmail.com>
2024-04-08 21:12:10 +0900

    feat/Shopモデルにバリデーション追加

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,9 @@
 class Shop < ApplicationRecord
   has_many :discounts, dependent: :destroy
+
+  validates :name, presence: true
+  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/ }
+  validates :address, presence: true
+  validates :latitude, numericality: true
+  validates :longitude, numericality: true
 end
