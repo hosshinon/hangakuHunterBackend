379bb6a7d677d56d4921c39985cb8a654949dfce
hosshinon <hosshinon@gmail.com>
2024-04-30 05:32:56 +0900

    fix/shopモデルのテスト文言を修正

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -1,40 +1,44 @@
 require "rails_helper"
 
-RSpec.describe Shop, type: :model do
+RSpec.describe Discount, type: :model do
   describe "validations" do
-    it "is valid with valid attributes" do
-      shop = build(:shop)
-      expect(shop).to be_valid
+    let(:shop) { create(:shop) }
+    let(:discount) do
+      build(:discount, shop:)
     end
 
-    it "is invalid without a name" do
-      shop = build(:shop, name: nil)
-      expect(shop).not_to be_valid
-      expect(shop.errors[:name]).to include("can't be blank")
+    it "有効な属性を持つ場合に有効である" do
+      expect(discount).to be_valid
     end
 
-    it "is invalid with an improperly formatted postal code" do
-      shop = build(:shop, postal_code: "1234567") # without hyphen
-      expect(shop).not_to be_valid
-      expect(shop.errors[:postal_code]).to include("is invalid")
+    it "タイトルがない場合に無効である" do
+      discount.title = nil
+      expect(discount).not_to be_valid
+      expect(discount.errors.full_messages).to include("Title can't be blank")
     end
 
-    it "is invalid without an address" do
-      shop = build(:shop, address: nil)
-      expect(shop).not_to be_valid
-      expect(shop.errors[:address]).to include("can't be blank")
+    it "割引率が0未満の場合に無効である" do
+      discount.discount_rate = -1
+      expect(discount).not_to be_valid
+      expect(discount.errors.full_messages).to include("Discount rate must be greater than or equal to 0")
     end
 
-    it "is invalid with a non-numeric latitude" do
-      shop = build(:shop, latitude: "not_a_number")
-      expect(shop).not_to be_valid
-      expect(shop.errors[:latitude]).to include("is not a number")
+    it "割引率が100を超える場合に無効である" do
+      discount.discount_rate = 101
+      expect(discount).not_to be_valid
+      expect(discount.errors.full_messages).to include("Discount rate must be less than or equal to 100")
     end
 
-    it "is invalid with a non-numeric longitude" do
-      shop = build(:shop, longitude: "not_a_number")
-      expect(shop).not_to be_valid
-      expect(shop.errors[:longitude]).to include("is not a number")
+    it "終了時間が開始時間より前の場合に無効である" do
+      discount.end_time = discount.start_time - 1.hour
+      expect(discount).not_to be_valid
+      expect(discount.errors.full_messages).to include("End time must be greater than #{discount.start_time}")
+    end
+
+    it "説明が長すぎる場合に無効である" do
+      discount.description = "a" * 256
+      expect(discount).not_to be_valid
+      expect(discount.errors.full_messages).to include("Description is too long (maximum is 255 characters)")
     end
   end
 end
