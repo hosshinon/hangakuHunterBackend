6923055fbf37b6f0fbf22685f71bcc966b437495
hosshinon <hosshinon@gmail.com>
2024-11-15 01:16:31 +0900

    fix/電話とウェブサイトのバリデーション削除

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -7,6 +7,4 @@ class Shop < ApplicationRecord
   validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
   validates :place_id, presence: true
   validates :formatted_address, presence: true
-  validates :international_phone_number, presence: true
-  validates :website, presence: true
 end
